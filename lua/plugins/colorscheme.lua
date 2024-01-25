2dbc6c7b70fd55e3b45d7ee500a014f2a393128d
Nuno Rodrigues <nunotechwork@gmail.com>
2024-01-25 15:39:08 +0000

    newupdate

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  { "xiantang/darcula-dark.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "darcula-dark",
+    },
+  },
+}
